78bfe334844a72014835a4c0a23e3887c76d33db
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2022-04-06 01:09:44 +0300

    feat: добавить поля почты и пароля, метод логина

--- models/user.js
+++ models/user.js
@@ -1,22 +1,57 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const isEmail = require('validator/es/lib/isEmail');
 
 const userSchema = new mongoose.Schema({
-  name: {
+  email: {
     type: String,
     required: true,
+    unique: true,
+    validate: {
+      validator: (email) => isEmail(email),
+      message: 'Неправильный формат почты',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  name: {
+    type: String,
+    default: 'Жак-Ив Кусто',
     minlength: 2,
     maxlength: 30,
   },
   about: {
     type: String,
-    required: true,
+    default: 'Жак-Ив Кусто',
     minlength: 2,
     maxlength: 30,
   },
   avatar: {
     type: String,
-    required: true,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
 });
+
+userSchema.statics.findUserByCredentials = function userLogin(email, password) {
+  this.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        // ошибка 'Пользователь с такой почтой не найден' код 401
+      }
+      // юзер найден
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            // ошибка 'Неправильные почта или пароль' код 401
+          }
+          // аутентификация успешна
+          // отправка данных пользователя
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
