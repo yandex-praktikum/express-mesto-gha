a62a8510f3e5e39cda47d39e39ba564efb344410
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2022-04-10 02:24:44 +0300

    fix: Вернуть объект пользователя при логине

--- controllers/users.js
+++ controllers/users.js
@@ -59,7 +59,7 @@ module.exports.login = (req, res, next) => {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
       })
-        .end();
+        .send(user);
     })
     .catch(next);
 };
