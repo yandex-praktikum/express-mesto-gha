f8846f2b008c7ed5e5b629c0fb80082572462a79
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2022-04-11 00:21:15 +0300

    style: Изменить порядок роутов

--- routes/users.js
+++ routes/users.js
@@ -12,21 +12,24 @@ const {
 // роуты пользователей
 router.get('/users', findUsers);
 router.get('/users/me', findUserById);
-router.get('/users/:userId', celebrate({
-  params: Joi.object().keys({
-    userId: Joi.string().length(24).hex(),
-  }),
-}), findUserById);
+
 router.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
   }),
 }), updateUser);
+
 router.patch('/users/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().required().pattern(regExp),
   }),
 }), updateAvatar);
+
+router.get('/users/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().length(24).hex(),
+  }),
+}), findUserById);
 
 module.exports = router;
