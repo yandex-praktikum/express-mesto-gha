5d34bf2615c78b333253c3ce9bf4996264c279d4
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2022-04-12 14:33:08 +0300

    fix: Исправить регулярку

--- utils/regex.js
+++ utils/regex.js
@@ -1,3 +1,3 @@
-const regExp = /^https?:\/\/(www)?[a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=]{1,}/;
+const regExp = /^https?:\/\/(www\.)?[a-zA-Z0-9\-.]{1,}\.[a-zA-Z]{1,4}[a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=]{1,}/;
 
 module.exports = regExp;
